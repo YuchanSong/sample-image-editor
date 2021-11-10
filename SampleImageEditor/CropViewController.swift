850e50c94503ff693d3c8ef3f6ad7cc361970e19
YuchanSong <dvlpr_uchan@kakao.com>
2021-11-10 21:18:55 +0900

    Refactoring & UI Update

--- SampleImageEditor/CropViewController.swift
+++ SampleImageEditor/CropViewController.swift
@@ -9,19 +9,13 @@ import UIKit
 
 protocol CropViewControllerDelegate {
     func cropViewController(_ controller: CropViewController, didFinishCroppingImage image: UIImage)
-    func cropViewController(_ controller: CropViewController, didFinishCroppingImage image: UIImage, transform: CGAffineTransform, cropRect: CGRect)
-    func cropViewControllerDidCancel(_ controller: CropViewController)
 }
 
 class CropViewController: UIViewController {
     private var delegate: CropViewControllerDelegate?
-    fileprivate var cropView: CropView?
+    private var cropView: CropView!
     
-    open var image: UIImage? {
-        didSet {
-            cropView?.image = image
-        }
-    }
+    private var image: UIImage?
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -34,53 +28,44 @@ class CropViewController: UIViewController {
         self.modalPresentationStyle = style
     }
     
-    open override func loadView() {
+    override func loadView() {
         let contentView: UIView = {
             let v = UIView()
             v.autoresizingMask = .flexibleWidth
             v.backgroundColor = UIColor.black
             return v
         }()
+
+        self.view = contentView
+        self.cropView = CropView(frame: contentView.bounds)
+        contentView.addSubview(cropView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        view = contentView
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(CropViewController.cancel(_:)))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(CropViewController.done(_:)))
         
-        // Add CropView
-        cropView = CropView(frame: contentView.bounds)
-        contentView.addSubview(cropView!)
+        self.cropView.image = image
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationController?.navigationBar.isTranslucent = false
-        navigationController?.toolbar.isTranslucent = false
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(CropViewController.cancel(_:)))
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(CropViewController.done(_:)))
-        
-//        if self.toolbarItems == nil {
-//            let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-//            let constrainButton = UIBarButtonItem(title: "Constrain", style: .plain, target: self, action: #selector(CropViewController.constrain(_:)))
-//            toolbarItems = [flexibleSpace, constrainButton, flexibleSpace]
-//        }
-        self.navigationController?.isToolbarHidden = false
-        
-        cropView?.image = image
     }
     
     @objc func cancel(_ sender: UIBarButtonItem) {
-        delegate?.cropViewControllerDidCancel(self)
+        self.navigationController?.popViewController(animated: true)
     }
     
     @objc func done(_ sender: UIBarButtonItem) {
-        if let image = cropView?.croppedImage {
+        self.navigationController?.popViewController(animated: true)
+        
+        if let image = cropView.croppedImage {
             delegate?.cropViewController(self, didFinishCroppingImage: image)
-            guard let rotation = cropView?.rotation else {
-                return
-            }
-            guard let rect = cropView?.zoomedCropRect() else {
-                return
-            }
-            delegate?.cropViewController(self, didFinishCroppingImage: image, transform: rotation, cropRect: rect)
+        } else {
+            print("img return error....")
         }
     }
 }
--- SampleImageEditor/ViewController.swift
+++ SampleImageEditor/ViewController.swift
@@ -10,90 +10,45 @@ import UIKit
 class ViewController: UIViewController {
     
     private let picker = UIImagePickerController()
-    
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var toolbar: UIToolbar!
+    
+    private func initPicker() {
+        self.picker.delegate = self
+        self.picker.sourceType = .photoLibrary
+        self.picker.modalPresentationStyle = .fullScreen
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.initPicker()
     }
     
-    @IBAction func getImageAction(_ sender: Any) {
-        self.openLibrary()
-    }
-    
-    @IBAction func cropAction(_ sender: Any) {
-        if let i = self.imageView.image {
-            let controller = CropViewController(delegate: self, image: i)
-            self.present(controller, animated: true, completion: nil)
-        } else {
-            // 이미지가 업서여..
-        }
-    }
-    
-    @IBAction func flipAction(_ sender: Any) {
-        self.imageView.transform = imageView.transform.rotated(by: CGFloat(Double.pi / 4)) //90 degree
-    }
-    
-    func openLibrary() {
-        picker.delegate = self
-        picker.sourceType = .photoLibrary
-        picker.modalPresentationStyle = .fullScreen
+    @IBAction func openLibrary(_ sender: Any) {
         self.present(picker, animated: true, completion: nil)
     }
 }
 
+//MARK: - UIImagePickerController Delegate
 extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            self.imageView.image = image
+            self.dismiss(animated: false, completion: {
+                let controller = CropViewController(delegate: self, image: image)
+                self.navigationController?.pushViewController(controller, animated: true)
+            })
         }
-        
-        self.dismiss(animated: false, completion: {
-            if let _ = self.imageView.image {
-                DispatchQueue.main.async {
-                    self.toolbar.isHidden ? self.toolbar.isHidden = false : nil
-                }
-            }
-        })
     }
 }
 
-extension UIImage {
-    func rotatedImageWithTransform(_ rotation: CGAffineTransform, croppedToRect rect: CGRect) -> UIImage {
-        let rotatedImage = rotatedImageWithTransform(rotation)
-        
-        let scale = rotatedImage.scale
-        let cropRect = rect.applying(CGAffineTransform(scaleX: scale, y: scale))
-        
-        let croppedImage = rotatedImage.cgImage?.cropping(to: cropRect)
-        let image = UIImage(cgImage: croppedImage!, scale: self.scale, orientation: rotatedImage.imageOrientation)
-        return image
-    }
-    
-    fileprivate func rotatedImageWithTransform(_ transform: CGAffineTransform) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(size, true, scale)
-        let context = UIGraphicsGetCurrentContext()
-        context?.translateBy(x: size.width / 2.0, y: size.height / 2.0)
-        context?.concatenate(transform)
-        context?.translateBy(x: size.width / -2.0, y: size.height / -2.0)
-        draw(in: CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height))
-        let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return rotatedImage!
-    }
-}
-
+//MARK: - CropViewController Delegate
 extension ViewController: CropViewControllerDelegate {
     func cropViewController(_ controller: CropViewController, didFinishCroppingImage image: UIImage) {
-        // TODO
-    }
-    
-    func cropViewController(_ controller: CropViewController, didFinishCroppingImage image: UIImage, transform: CGAffineTransform, cropRect: CGRect) {
-        // TODO
-    }
-    
-    func cropViewControllerDidCancel(_ controller: CropViewController) {
-        // TODO
+        self.imageView.image = image
     }
 }
